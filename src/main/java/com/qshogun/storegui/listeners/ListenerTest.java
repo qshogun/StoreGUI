f9ac50a145e54da7b5b10c7c7f660450aa56f111
qshogun <55805051+qshogun@users.noreply.github.com>
2019-12-29 22:33:33 +0100

    created Listeners class added methods from TestSuccess and TestFailure

---
+++ src/main/java/com/qshogun/storegui/listeners/ListenerTest.java
@@ -0,0 +1,45 @@
+package com.qshogun.storegui.listeners;
+
+import com.qshogun.storegui.util.AddResultForTestCase;
+import com.qshogun.storegui.util.HelperMethods;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenerTest implements ITestListener {
+    public void onTestStart(ITestResult result) {
+
+    }
+
+    public void onTestSuccess(ITestResult result) {
+        String testCaseId = HelperMethods.getTestCaseId(result.getName());
+        AddResultForTestCase.addPass(testCaseId);
+
+    }
+
+    public void onTestFailure(ITestResult result) {
+        String testCaseId = HelperMethods.getTestCaseId(result.getName());
+        AddResultForTestCase.addFail(testCaseId,
+                result.getName()+" test failed.");
+    }
+
+    public void onTestSkipped(ITestResult result) {
+
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+
+    }
+
+    public void onTestFailedWithTimeout(ITestResult result) {
+
+    }
+
+    public void onStart(ITestContext context) {
+
+    }
+
+    public void onFinish(ITestContext context) {
+
+    }
+}
